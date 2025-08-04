d944c037f52d9c13ac1f7f347098d31117dac43a
Colin Ugo <ugo.colin.062@gmail.com>
2025-08-04 14:03:23 +0200

    maj

--- src/data/carousel.js
+++ src/data/carousel.js
@@ -1,112 +1,112 @@
 export const carouselImages = [
     {
         id: 1,
-        src: "projects/carousel/1.png",
+        src: "projects/carousel/1.webp",
         alt: "Réalisation 1"
     },
     {
         id: 2,
-        src: "projects/carousel/2.png",
+        src: "projects/carousel/2.webp",
         alt: "Réalisation 2"
     },
     {
         id: 3,
-        src: "projects/carousel/3.jpg",
+        src: "projects/carousel/3.webp",
         alt: "Réalisation 3"
     },
     {
         id: 4,
-        src: "projects/carousel/4.png",
+        src: "projects/carousel/4.webp",
         alt: "Réalisation 4"
     },
     {
         id: 5,
-        src: "projects/carousel/5.png",
+        src: "projects/carousel/5.webp",
         alt: "Réalisation 5"
     },
     {
         id: 7,
-        src: "projects/carousel/7.jpg",
+        src: "projects/carousel/7.webp",
         alt: "Réalisation 7"
     },
     {
         id: 8,
-        src: "projects/carousel/8.jpg",
+        src: "projects/carousel/8.webp",
         alt: "Réalisation 8"
     },
     {
         id: 9,
-        src: "projects/carousel/9.png",
+        src: "projects/carousel/9.webp",
         alt: "Réalisation 9"
     },
     {
         id: 10,
-        src: "projects/carousel/10.png",
+        src: "projects/carousel/10.webp",
         alt: "Réalisation 10"
     },
     {
         id: 11,
-        src: "projects/carousel/11.png",
+        src: "projects/carousel/11.webp",
         alt: "Réalisation 11"
     },
     {
         id: 13,
-        src: "projects/carousel/13.JPG",
+        src: "projects/carousel/13.webp",
         alt: "Réalisation 13"
     },
     {
         id: 14,
-        src: "projects/carousel/14.JPG",
+        src: "projects/carousel/14.webp",
         alt: "Réalisation 14"
     },
     {
         id: 16,
-        src: "projects/carousel/16.png",
+        src: "projects/carousel/16.webp",
         alt: "Réalisation 16"
     },
     {
         id: 17,
-        src: "projects/carousel/17.png",
+        src: "projects/carousel/17.webp",
         alt: "Réalisation 17"
     },
     {
         id: 18,
-        src: "projects/carousel/18.png",
+        src: "projects/carousel/18.webp",
         alt: "Réalisation 18"
     },
     {
         id: 19,
-        src: "projects/carousel/19.png",
+        src: "projects/carousel/19.webp",
         alt: "Réalisation 19"
     },
     {
         id: 20,
-        src: "projects/carousel/20.png",
+        src: "projects/carousel/20.webp",
         alt: "Réalisation 20"
     },
     {
         id: 21,
-        src: "projects/carousel/21.png",
+        src: "projects/carousel/21.webp",
         alt: "Réalisation 21"
     },
     {
         id: 22,
-        src: "projects/carousel/22.png",
+        src: "projects/carousel/22.webp",
         alt: "Réalisation 22"
     },
     {
         id: 23,
-        src: "projects/carousel/23.png",
+        src: "projects/carousel/23.webp",
         alt: "Réalisation 23"
     },
     {
         id: 24,
-        src: "projects/carousel/24.png",
+        src: "projects/carousel/24.webp",
         alt: "Réalisation 24"
     },
     {
         id: 25,
-        src: "projects/carousel/25.png",
+        src: "projects/carousel/25.webp",
         alt: "Réalisation 25"
     }
 ];
